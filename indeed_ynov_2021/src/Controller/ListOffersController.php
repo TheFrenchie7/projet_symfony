5c7b41227759c61972ecdfdccec318419969b35b
Tristan Lefebvre <tristan.lefebvre@ynov.com>
2020-11-24 14:48:10 +0100

    add current progression of form

--- indeed_ynov_2021/src/Controller/ListOffersController.php
+++ indeed_ynov_2021/src/Controller/ListOffersController.php
@@ -4,7 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Offre;
 use App\Repository\OffreRepository;
+use Doctrine\DBAL\Types\DateType;
+use Doctrine\DBAL\Types\SmallIntType;
+use Doctrine\DBAL\Types\TextType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,6 +36,37 @@ class ListOffersController extends AbstractController
         return $this->render('offres/offre.html.twig', [
             'offre' => $offre
         ]);
+    }
+
+    /**
+     * @Route("/offres/create", name="create_offre")
+     */ 
+    public function create(Offre $offre, EntityManagerInterface $em, Request $request)
+    {
+        $offre = new Offre();
+
+        $form = $this->createFormBuilder($offre)
+            ->add("title")
+            ->add("description", TextType::class)
+            ->add("addresse")
+            ->add("postcode", SmallIntType::class)
+            ->add("ville")
+            ->add("end", DateType::class)
+            ->add("contrat", )
+            ->add("type", )
+            ->add("Submit", SubmitType::class)
+            ->getForm();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($offre);
+            $em->flush();
+
+            $this->redirectToRoute("/");
+        }
+        
+        return $this->render('offres/create.html.twig', [
+            'form' => $form
+        ]);        
     }
 
 }
